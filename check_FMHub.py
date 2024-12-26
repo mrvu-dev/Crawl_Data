9a854dfc672a1b5bc017689462e59523b537786d
Vu Nguyen <mrvu.nguyenduy@gmail.com>
2024-12-27 04:54:12 +0700

    test github setup

---
+++ check_FMHub.py
@@ -0,0 +1,7 @@
+
+{
+  "order_status": "42",
+  "count": 50,
+  "current_station_ids": "915",
+  "page_no": 1
+}
--- cookies.py
+++ cookies.py
@@ -2,7 +2,7 @@ def get_cookies():
     cookies = {
         'fms_user_id': '3963',
         # ---------------------
-        'fms_user_skey': 'ol0GOf3sXRRF6C3mrq6V8LZeIyw2XP1o5OWn4phG3603YDsDK7CvOVJ5ScpxcDKy',
+        'fms_user_skey': 'kn1KFISr5u38xEBsX0P0IF0ogEs8K0REcs7jn5D8ycVdwknxH4k9R3C2ZkBsWcOO',
         # ---------------------
         'fms_display_name': 'vu.nguyenduy',
         'spx_st': '1',
